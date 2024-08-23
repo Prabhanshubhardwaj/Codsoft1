e59c2b2ea4968362689ffbb6feedcf16268c033b
Prabhanshubhardwaj <prabhanshubhardwaj2003@gmail.com>
2024-08-23 19:55:15 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,54 @@
+ //TASK 1 CODSOFT......
+
+#include <iostream>
+using namespace std;
+
+void showChoices() {
+    cout << "Choose an operation to perform:"<<endl;
+    cout << "1. Addition"<<endl;
+    cout << "2. Subtraction"<<endl;
+    cout << "3. Multiplication"<<endl;
+    cout << "4. Division"<<endl;
+    cout << "Enter your choice (1-4): ";
+}
+
+int main() {
+    double num1, num2, output;
+    int choice;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    showChoices();
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            output = num1 + num2;
+            cout << "output: " << num1 << " + " << num2 << " = " << output << endl;
+            break;
+        case 2:
+            output = num1 - num2;
+            cout << "output: " << num1 << " - " << num2 << " = " << output << endl;
+            break;
+        case 3:
+            output = num1 * num2;
+            cout << "output: " << num1 << " * " << num2 << " = " << output << endl;
+            break;
+        case 4:
+            if (num2 != 0) {
+                output = num1 / num2;
+                cout << "output: " << num1 << " / " << num2 << " = " << output << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid choice. Please select a valid operation (1-4)." << endl;
+            break;
+    }
+
+    return 0;
+}
